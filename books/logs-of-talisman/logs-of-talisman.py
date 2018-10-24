d2c9f9c295285acac577a5a072307cde6202e478
Eli Heuer <eliheuer@gmail.com>
2018-10-23 20:10:31 -0400

    Add basic pagebot file

---
+++ books/logs-of-talisman/logs-of-talisman.py
@@ -0,0 +1,33 @@
+
+email_001 = '''
+
+From jrcole@umich.eduTue Oct  3 12:14:12 1995
+Date: Mon, 4 Sep 1995 12:17:22 -0400 (EDT)
+From: Juan R Cole <jrcole@umich.edu>
+To: gpoirier <gpoirier@acca.nmsu.edu>
+Subject: Re: Evolution of authority
+
+
+Brent:  The distinction between Revelation (vahy) and inspiration (ilham) 
+is very strong in the original languages, but it is not so stark in 
+English.  Thus, the beloved Guardian occasionally translates vahy as 
+inspiration, though mostly as revelation.  And occasionally "ilham" or 
+inspiration is rendered as revelation.
+
+This is what is going on when Shoghi Effendi speaks of `Abdu'l-Baha as 
+"revealing" a Tablet.  It is a matter of loose translation into a 
+language with different resonances.  I guarantee you that he does not 
+mean "vahy" by this language and that in Arabic or Persian the beloved 
+Guardian *never* associated "vahy" or divine Revelation with `Abdu'l-Baha 
+or indeed with anyone but a Manifestation of God.
+
+Likewise, no Baha'i text bestows on `Abdu'l-Baha the right of divine 
+Legislation (shari`ah), and the Master would have been horrified at the 
+thought.
+
+
+cheers   Juan Cole, History, University of Michigan
+
+'''
+
+print(email_001)
